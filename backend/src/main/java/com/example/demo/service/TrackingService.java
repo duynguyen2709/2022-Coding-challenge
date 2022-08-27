9bb593c17da682e4557f9f5427c7dc77b4cbfe70
thongphn <thongphn@vng.com.vn>
2022-08-27 11:49:47 +0700

    fix logic

--- backend/src/main/java/com/example/demo/service/TrackingService.java
+++ backend/src/main/java/com/example/demo/service/TrackingService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.Class.Incident;
 import com.example.demo.Class.Location;
-import com.example.demo.Class.Offcier;
+import com.example.demo.Class.Officer;
 import com.example.demo.wrapper.BaseResponse;
 import com.example.demo.wrapper.MetricPhase2;
 import org.springframework.stereotype.Service;
@@ -22,10 +22,10 @@ public class TrackingService {
         incidentsList.add(incidentA);
         incidentsList.add(incidentB);
 
-        Offcier offcierA = new Offcier(1, "Thong 1", new Location(1, 1));
-        Offcier offcierB = new Offcier(2, "Thong 2", new Location(4, 4));
-        Offcier offcierC = new Offcier(3, "Thong 3", new Location(5, 5));
-        List<Offcier> offcierList = new ArrayList<>();
+        Officer offcierA = new Officer(1, "Thong 1", new Location(1, 1));
+        Officer offcierB = new Officer(2, "Thong 2", new Location(4, 4));
+        Officer offcierC = new Officer(3, "Thong 3", new Location(5, 5));
+        List<Officer> offcierList = new ArrayList<>();
         offcierList.add(offcierA);
         offcierList.add(offcierB);
         offcierList.add(offcierC);
@@ -39,7 +39,7 @@ public class TrackingService {
 
     private LinkedHashMap<Integer, Integer> getNearestOfficer(
             List<Incident> incidentList,
-            List<Offcier> offcierList
+            List<Officer> offcierList
     ) {
         LinkedHashMap<Integer, Integer> result = new LinkedHashMap<>();
 
