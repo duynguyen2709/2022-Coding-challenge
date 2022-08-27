61b558ae714aab9175d7afc11fe7c672a3ef113a
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-08-27 10:53:44 +0700

    ADD class incident and officer

---
+++ backend/src/main/java/com/example/demo/Class/Offcier.java
@@ -0,0 +1,37 @@
+package com.example.demo.Class;
+
+public class Offcier {
+    private Integer id;
+    private String badgeName;
+    private Location loc;
+
+    public Offcier(Integer id, String badgeName, Location loc) {
+        this.id = id;
+        this.badgeName = badgeName;
+        this.loc = loc;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+
+    public void setBadgeName(String badgeName) {
+        this.badgeName = badgeName;
+    }
+
+    public Location getLoc() {
+        return loc;
+    }
+
+    public void setLoc(Location loc) {
+        this.loc = loc;
+    }
+}
